7cc841329f0ad969e1ce6a46f43a40a7af8a2574
Roberto Cabal <robertocabal5@gmail.com>
2022-10-18 11:32:01 -0500

    firts terraform main

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0688ba7eeeeefe3cd"
+    instance_type= "t2.micro"
+    tags = {
+        Name = "terraform-example"
+    }
+}
