6771b8959f640d34323f2a3b6a8c7706c5e406ab
Roberto Cabal <robertocabal5@gmail.com>
2022-10-20 08:46:29 -0500

    modify terraform main

--- main.tf
+++ main.tf
@@ -1,29 +1,109 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "REPLACE_ME"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0688ba7eeeeefe3cd"
-    instance_type= "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+resource "random_pet" "sg" {}
 
-    user_data = <<-EOF
-    #!bin/bash
-    echo "Hello World!!" > index.html
-    nohup busybox httpd -f -p 8080 &
-    EOF
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-    tags = {
-        Name = "terraform-example"
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
 }
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
+
+# provider "aws" {
+#     region = "us-west-2"
+# }
+
+# resource "aws_instance" "example" {
+#     ami = "ami-0688ba7eeeeefe3cd"
+#     instance_type= "t2.micro"
+#     vpc_security_group_ids = [aws_security_group.instance.id]
+
+#     user_data = <<-EOF
+#     #!bin/bash
+#     echo "Hello World!!" > index.html
+#     nohup busybox httpd -f -p 8080 &
+#     EOF
+
+#     tags = {
+#         Name = "terraform-example"
+#     }
+# }
+
+# resource "aws_security_group" "instance" {
+#     name = "terraform-example-instance"
+#     ingress {
+#         from_port = 8080
+#         to_port = 8080
+#         protocol = "tcp"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
+# }
