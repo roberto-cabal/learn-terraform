dacbb69f18d5ed6671abce17dcc7b4650331b511
Roberto Cabal <robertocabal5@gmail.com>
2022-10-21 08:28:03 -0500

    add pet and variables

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# resource "random_pet" "sg" {}
+resource "random_pet" "sg" {}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -42,6 +42,12 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+variable "server_port" {
+  description = "The port the server will use for http requests"
+  type = number
+  default = 8080
+}
+
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
@@ -51,7 +57,7 @@ resource "aws_instance" "web" {
               #!/bin/bash
               apt-get update
               apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              sed -i -e 's/80/${var.server_port}/' /etc/apache2/ports.conf
               echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
               EOF
@@ -60,8 +66,8 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -75,35 +81,5 @@ resource "aws_security_group" "web-sg" {
 }
 
 output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+  value = "${aws_instance.web.public_dns}:${var.server_port}"
 }
-
-# provider "aws" {
-#     region = "us-west-2"
-# }
-
-# resource "aws_instance" "example" {
-#     ami = "ami-0688ba7eeeeefe3cd"
-#     instance_type= "t2.micro"
-#     vpc_security_group_ids = [aws_security_group.instance.id]
-
-#     user_data = <<-EOF
-#     #!bin/bash
-#     echo "Hello World!!" > index.html
-#     nohup busybox httpd -f -p 8080 &
-#     EOF
-
-#     tags = {
-#         Name = "terraform-example"
-#     }
-# }
-
-# resource "aws_security_group" "instance" {
-#     name = "terraform-example-instance"
-#     ingress {
-#         from_port = 8080
-#         to_port = 8080
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-# }
